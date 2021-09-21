fae4cbdbbe1257a5a48c7d6bbb214115074e1690
Stas <stastmn@gmail.com>
2021-09-21 11:57:51 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace refs
+{
+    internal static class Program
+    {
+        public static void Main(string[] gitDirectoryPath)
+        {
+            try
+            {
+                if (gitDirectoryPath.Length != 1) throw new Exception();
+                RefsPrinter.PrintRefs(gitDirectoryPath[0]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ RefsPrinter.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+
+namespace refs
+{
+    public static class RefsPrinter
+    {
+        private static string _gitPath = @".\\.git";
+        private static string _gitDirectoryPath;
+
+        public static void PrintRefs(string gitDirectoryPath)
+        {
+            _gitDirectoryPath = gitDirectoryPath;
+            _gitPath = gitDirectoryPath + _gitPath;
+            if (!IsGitDirectoryExists()) throw new Exception("Cannot find \".git\" catalog.");
+            if (!IsGitRefsDirectoryExists())
+                throw new Exception("Cannot find \"refs\" catalog in \".git\".");
+
+            ProcessHeads();
+            ProcessRemotes();
+            ProcessTags();
+            ProcessPackedRefs();
+        }
+
+        private static void ProcessPackedRefs()
+        {
+            var packedRefsPath = _gitDirectoryPath +  @".\\.git\\packed-refs";
+            if (!File.Exists(packedRefsPath)) return;
+            using (var streamReader = new StreamReader(packedRefsPath))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    var content = streamReader.ReadLine();
+                    if (content is null || content.Length < 1) continue;
+                    switch (content[0])
+                    {
+                        case '#':
+                        //TODO:need process referenced objects?
+                        case '^':
+                            continue;
+                        default:
+                        {
+                            var contentAndName = content.Split(' ');
+                            Console.WriteLine(contentAndName[1] + "\t" + contentAndName[0]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void ProcessTags()
+        {
+            var tagsPath = _gitPath + "\\refs\\tags";
+            if (!Directory.Exists(tagsPath)) return;
+            var files = Directory.GetFiles(tagsPath);
+
+            foreach (var file in files)
+            {
+                var content = File.ReadAllText(file);
+                var tagsName = file.Replace(_gitPath, "");
+                Console.WriteLine(tagsName + "\t" + content.Replace("\n", ""));
+            }
+        }
+
+        private static void ProcessRemotes()
+        {
+            var remotesPath = _gitPath + "\\refs\\remotes";
+            if (!Directory.Exists(remotesPath)) return;
+            var remotes = Directory.GetDirectories(remotesPath);
+            foreach (var remote in remotes)
+            {
+                var files = Directory.GetFiles(remote);
+                foreach (var file in files)
+                {
+                    var content = File.ReadAllText(file);
+                    var remoteName = file.Replace(_gitPath, "");
+                    Console.WriteLine(remoteName + "\t" + content.Replace("\n", ""));
+                }
+            }
+        }
+
+        private static bool IsGitRefsDirectoryExists()
+        {
+            return Directory.Exists(_gitDirectoryPath + @".\\.git\\refs");
+        }
+
+        private static bool IsGitDirectoryExists()
+        {
+            return Directory.Exists(_gitDirectoryPath + @".\\.git");
+        }
+
+        private static void ProcessHeads()
+        {
+            var headsPath = _gitPath + "\\refs\\heads";
+            if (!Directory.Exists(headsPath)) return;
+            var files = Directory.GetFiles(headsPath);
+
+            foreach (var file in files)
+            {
+                var content = File.ReadAllText(file);
+                var refName = file.Replace(_gitPath, "");
+                Console.WriteLine(refName + "\t" + content.Replace("\n", ""));
+            }
+        }
+    }
+}
